b9c2171f1da042095f0b9432674b5ffd070e164c
Kimberly Gonzalez <kgonzalez1718@gmail.com>
2022-12-03 19:47:38 -0600

    update and delete user info data records from table
    Can now update or delete a user's info data record by first viewing the users records available right now, entering an available user's id number into the select user id text box -> then either hit delete or edit to do those corresponding actions to the table. Currently only have admin user record and an adam smith record

--- DB_Connect.py
+++ DB_Connect.py
@@ -4,7 +4,7 @@ import sqlite3
 
 root = Tk()
 root.title('Theater E-Ticket Info')
-root.geometry("400x400")
+root.geometry("500x600")
 
 # database------------------
 
@@ -27,6 +27,151 @@ c.execute("""CREATE TABLE USER_INFO (
         USER_HIST money
         )""")
 '''
+
+def update():
+    conn = sqlite3.connect('theater_ticketing.db')
+    c = conn.cursor()
+
+    record_select_id = delete_box.get()
+    c.execute("""UPDATE USER_INFO SET
+        USER_ID = :ID,
+        USER_FNAME = :FIRST,
+        USER_LNAME = :LAST,
+        USER_PHONE = :PHONE,
+        USER_EMAIL = :EMAIL,
+        USER_AGE = :AGE,
+        USER_PAY = :PAY, 
+        USER_HIST = :HIST
+        
+        WHERE USER_ID = :SELECTED_ID""",
+        {
+        'ID': user_id_edit.get(),
+        'FIRST': first_name_edit.get(),
+        'LAST': last_name_edit.get(),
+        'PHONE': phone_num_edit.get(),
+        'EMAIL': email_addr_edit.get(),
+        'AGE': age_edit.get(),
+        'PAY': pay_edit.get(),
+        'HIST': history_edit.get(),
+        'SELECTED_ID': record_select_id
+        })
+
+    conn.commit()
+    conn.close()
+
+    editor.destroy()
+
+
+
+# edit/updates a user's info record
+def edit():
+    global editor
+    editor = Tk()
+    editor.title('Edit User Record Info')
+    editor.geometry("550x400")
+    conn = sqlite3.connect('theater_ticketing.db')
+    c = conn.cursor()
+
+    record_id = delete_box.get()
+
+    # query here means everything and the primary key
+    c.execute("SELECT * FROM USER_INFO WHERE USER_ID =" + record_id)
+
+    user_records = c.fetchall()
+
+    # create the text box namee edits to global variables
+    global user_id_edit
+    global first_name_edit
+    global last_name_edit
+    global phone_num_edit
+    global email_addr_edit
+    global age_edit
+    global pay_edit
+    global history_edit
+
+    # creating the text boxes ~~~~~~~~~~~~~~~~~~~~~
+    user_id_edit = Entry(editor, width=30)
+    user_id_edit.grid(row=0, column=1, padx=20, pady=(10, 0))
+
+    first_name_edit = Entry(editor, width=30)
+    first_name_edit.grid(row=1, column=1, padx=20)
+
+    last_name_edit = Entry(editor, width=30)
+    last_name_edit.grid(row=2, column=1)
+
+    phone_num_edit = Entry(editor, width=30)
+    phone_num_edit.grid(row=3, column=1)
+
+    email_addr_edit = Entry(editor, width=30)
+    email_addr_edit.grid(row=4, column=1)
+
+    age_edit = Entry(editor, width=30)
+    age_edit.grid(row=5, column=1)
+
+    pay_edit = Entry(editor, width=30)
+    pay_edit.grid(row=6, column=1)
+
+    history_edit = Entry(editor, width=30)
+    history_edit.grid(row=7, column=1)
+
+    # creating labels for the text boxes ~~~~~~~~~~~~~~~
+    user_id_label = Label(editor, text="User ID")
+    user_id_label.grid(row=0, column=0, pady=(10, 0))
+
+    first_name_label = Label(editor, text="First Name")
+    first_name_label.grid(row=1, column=0)
+
+    last_name_label = Label(editor, text="Last Name")
+    last_name_label.grid(row=2, column=0)
+
+    phone_num_label = Label(editor, text="Phone Number")
+    phone_num_label.grid(row=3, column=0)
+
+    email_addr_label = Label(editor, text="Email Address")
+    email_addr_label.grid(row=4, column=0)
+
+    age_label = Label(editor, text="Age")
+    age_label.grid(row=5, column=0)
+
+    pay_label = Label(editor, text="Payment Card")
+    pay_label.grid(row=6, column=0)
+    # maybe dont allow admin to edit the payment???
+    # TODO add the payments to history automatically
+    history_label = Label(editor, text="Payment History")
+    history_label.grid(row=7, column=0)
+
+    # go through our record data and update them by looping
+    for record in user_records:
+        user_id_edit.insert(0, record[0])
+        first_name_edit.insert(0, record[1])
+        last_name_edit.insert(0, record[2])
+        phone_num_edit.insert(0, record[3])
+        email_addr_edit.insert(0, record[4])
+        age_edit.insert(0, record[5])
+        pay_edit.insert(0, record[6])
+        history_edit.insert(0, record[7])
+
+    # creates a save button to save the edits/updates done on the user's record information
+    save_button = Button(editor, text="Save User's Info Records", command=update)
+    save_button.grid(row=8, column=0, columnspan=2, pady=10, padx=10, ipadx=135)
+
+    # conn.commit()
+    # conn.close()
+
+
+# create a function that deletes a user's info record from the user_info table
+def delete():
+    conn = sqlite3.connect('theater_ticketing.db')
+    c = conn.cursor()
+
+    # delete a user record
+    c.execute("DELETE from USER_INFO WHERE USER_ID = " + delete_box.get())
+
+    delete_box.delete(0, END)
+
+    conn.commit()
+    conn.close()
+
 
 # creating the submit function for the submit_button
 def submit():
@@ -59,24 +204,26 @@ def submit():
     pay.delete(0, END)
     history.delete(0, END)
 
-#create a sql query function for the query button
+
+# create a sql query function for the query button
 def query():
     conn = sqlite3.connect('theater_ticketing.db')
     c = conn.cursor()
 
-    #oid here means everything and the primary key
-    c.execute("SELECT *, oid FROM USER_INFO")
+    # query here means everything and the primary key
+    c.execute("SELECT * FROM USER_INFO")
 
-    User_Records = c.fetchall()
+    user_records = c.fetchall()
     # print(User_Records)
     print_records = ''
 
 # looping through all user info 'show record' query
-    for record in User_Records:
-        print_records += str(record) +" "
+    for record in user_records:
+        print_records += str(record[0]) + "  " + str(record[1]) + "  " + str(record[2]) + "  " + str(record[3]) + "  " \
+                         + str(record[4]) + "  " + str(record[5]) + "  " + str(record[6]) + "  " + str(record[7]) + " \n"
 
     query_label = Label(root, text=print_records)
-    query_label.grid(row=10, column=0, columnspan=2)
+    query_label.grid(row=11, column=0, columnspan=2)
 
     conn.commit()
     conn.close()
@@ -84,7 +231,7 @@ def query():
 
 # creating the text boxes ~~~~~~~~~~~~~~~~~~~~~
 user_id = Entry(root, width=30)
-user_id.grid(row=0, column=1, padx=20)
+user_id.grid(row=0, column=1, padx=20, pady=(10,0))
 
 first_name = Entry(root, width=30)
 first_name.grid(row=1, column=1, padx=20)
@@ -107,9 +254,12 @@ pay.grid(row=6, column=1)
 history = Entry(root, width=30)
 history.grid(row=7, column=1)
 
+delete_box = Entry(root, width=30)
+delete_box.grid(row=12, column=1, pady=5)
+
 # creating labels for the text boxes ~~~~~~~~~~~~~~~
 user_id_label = Label(root, text="User ID")
-user_id_label.grid(row=0, column=0)
+user_id_label.grid(row=0, column=0, pady=(10, 0))
 
 first_name_label = Label(root, text="First Name")
 first_name_label.grid(row=1, column=0)
@@ -133,13 +283,24 @@ pay_label.grid(row=6, column=0)
 history_label = Label(root, text="Payment History")
 history_label.grid(row=7, column=0)
 
+delete_label = Label(root, text="Select User ID")
+delete_label.grid(row=12, column=0, pady=5)
+
 # create a submit button ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 submit_button = Button(root, text="Add User Info Record To Database", command=submit)
-submit_button.grid(row=8, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+submit_button.grid(row=8, column=0, columnspan=2, pady=10, padx=10, ipadx=120)
 
 # create a query button to see the user table information
 query_button = Button(root, text="Show User Info Records", command=query)
-query_button.grid(row=9, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
+query_button.grid(row=9, column=0, columnspan=2, pady=10, padx=10, ipadx=147)
+
+# create a delete button to delete a user's record from the table
+delete_button = Button(root, text="Delete a User's Info Records", command=delete)
+delete_button.grid(row=13, column=0, columnspan=2, pady=10, padx=10, ipadx=135)
+
+# create an edit button to update/edit a user's info record
+edit_button = Button(root, text="Edit a User's Info Records", command=edit)
+edit_button.grid(row=14, column=0, columnspan=2, pady=10, padx=10, ipadx=142)
 
 
 
