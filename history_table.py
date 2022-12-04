f542f225b9e27acc0f72fbd8a35d9edb0e558251
Kimberly Gonzalez <kgonzalez1718@gmail.com>
2022-12-04 13:59:36 -0600

    changes in history file, added tables
    in the history table: added the movie and ticket table with correct fks. Nothing can be run other than the root pop up window. Changes in the DB

--- history_table.py
+++ history_table.py
@@ -11,9 +11,58 @@ root.geometry("500x600")
 conn = sqlite3.connect('theater_ticketing.db')
 
 # cursor creation
-c = conn.cursor()
+#c = conn.cursor()
 
 # creating table for Theater info ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+query = (''' CREATE TABLE IF NOT EXISTS MOVIE
+            (MOVIE_ID INTEGER PRIMARY KEY,
+            MOVIE_TITLE TEXT,
+            MOVIE_SCREEN INTEGER,
+            MOVIE_DATE TEXT,
+            MOVIE_START INTEGER,
+            MOVIE_END INTEGER,
+            MOVIE_RATING INTEGER,
+            MOVIE_PRICE MONEY,
+            THEAT_ID INTEGER NOT NULL,
+            FOREIGN KEY (THEAT_ID) REFERENCES THEATER(THEATER_ID)
+            );''')
+conn.execute(query)
+
+
+conn.execute("PRAGMA foreign_keys = ON")
+
+conn.execute("INSERT INTO THEATER (THEATER_ID,THEATER_NAME,THEATER_SEATCAPACITY) \
+VALUES (2, 'Test Cinema', 34)");
+
+conn.execute("INSERT INTO MOVIE (MOVIE_ID, MOVIE_TITLE, MOVIE_SCREEN, MOVIE_DATE, MOVIE_START, MOVIE_END, MOVIE_RATING, MOVIE_PRICE, THEAT_ID) \
+VALUES (1, 'TEST MOVIE', 2, 'OCT 12 22', 1200, 1400, 7, 12.99, 2)");
+
+
+
+query = (''' CREATE TABLE IF NOT EXISTS TICKET
+            (TICKET_NUM INTEGER primary key,
+            CONF_NUM INTEGER,
+            SEAT_NUM INTEGER,
+            USER_ID integer,
+            THEATER_ID INTEGER NOT NULL,
+            MOVIE_ID INTEGER,       
+            FOREIGN KEY (USER_ID) REFERENCES USER_INFO(USER_ID), 
+            FOREIGN KEY (THEATER_ID) REFERENCES THEATER(THEATER_ID),
+            FOREIGN KEY (MOVIE_ID) REFERENCES MOVIE(MOVIE_ID)
+            );''')
+
+conn.execute(query)
+
+
+
+conn.execute("PRAGMA foreign_keys = ON")
+
+conn.execute("INSERT INTO TICKET (TICKET_NUM, CONF_NUM, SEAT_NUM, USER_ID, THEATER_ID, MOVIE_ID) \
+VALUES (123, 321, 23, 1, 2, 1)");
+
+"""
+
 '''
 c.execute("""CREATE TABLE MOVIE (
         MOVIE_ID integer primary key,
@@ -41,7 +90,7 @@ c.execute("""CREATE TABLE TICKET (
         FOREIGN KEY (THEATER_ID) REFERENCES THEATER(THEATER_ID),
         FOREIGN KEY (MOVIE_SCREEN) REFERENCES MOVIE(MOVIE_SCREEN),
         FOREIGN KEY (USER_ID) REFERENCES USER_INFO(USER_ID),
-        FOREIGN KEY (MOVIE_ID) REFERENCES MOVIE(MOVIE_ID),
+        FOREIGN KEY (MOVIE_ID) REFERENCES MOVIE(MOVIE_ID)
         )""")
 
 c.execute("""CREATE TABLE HISTORY (
